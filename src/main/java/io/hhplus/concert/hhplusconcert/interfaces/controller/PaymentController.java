45208b438a696ca747909ee17ae565309b667268
leesangjun <hubls@naver.com>
2025-01-10 00:05:03 +0900

    🔨 Fix: PaymentController에 HTTP Method 누락한 것 추가

--- src/main/java/io/hhplus/concert/hhplusconcert/interfaces/controller/PaymentController.java
+++ src/main/java/io/hhplus/concert/hhplusconcert/interfaces/controller/PaymentController.java
@@ -6,10 +6,7 @@ import io.hhplus.concert.hhplusconcert.interfaces.dto.PaymentDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -20,6 +17,7 @@ public class PaymentController {
     private final PaymentFacade paymentFacade;
 
     // 결제 요청
+    @PostMapping
     public ResponseEntity<PaymentDto.PaymentResponse> requestPayment(
             @RequestHeader("Token") String token,
             @Valid @RequestBody PaymentDto.PaymentRequest request
