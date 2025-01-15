1a4e9e6884e591b160f00dde8dd4ca7e2b87e266
leesangjun <hubls@naver.com>
2025-01-15 13:49:34 +0900

    🚨 Test: 활성 토큰이 200개(MAX_ACTIVE_TOKENS)미만 이면 WAITING -> ACTIVE 토큰으로 전환하는 기능 테스트

--- src/test/java/io/hhplus/concert/hhplusconcert/domain/service/QueueServiceTest.java
+++ src/test/java/io/hhplus/concert/hhplusconcert/domain/service/QueueServiceTest.java
@@ -5,13 +5,17 @@ import io.hhplus.concert.hhplusconcert.domain.repository.QueueRepository;
 import io.hhplus.concert.hhplusconcert.support.type.QueueStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class QueueServiceTest {
@@ -73,4 +77,43 @@ class QueueServiceTest {
         assertThat(actualToken.token()).isEqualTo(expectedToken.token());
         assertThat(actualToken.status()).isEqualTo(QueueStatus.WAITING);
     }
+
+    @Test
+    void 활성_토큰이_200개_미만이면_대기열_토큰을_활성_상태로_변경한다() {
+        // given
+        long activeTokenCount = 150L; // 현재 활성화된 토큰 개수
+        long maxActiveTokens = 200L; // 최대 활성화 가능한 토큰 개수
+        long neededTokens = maxActiveTokens - activeTokenCount; // 필요한 토큰 개수
+
+        Queue token = Queue.builder()
+                .id(1L)
+                .userId(123L)
+                .token("1213124")
+                .status(QueueStatus.WAITING)
+                .createdAt(LocalDateTime.now())
+                .enteredAt(LocalDateTime.now())
+                .expiredAt(LocalDateTime.now())
+                .build();
+
+        // 필요한 토큰 목록을 반환하는 mock
+        List<Queue> waitingTokens = List.of(token);
+
+        when(queueRepository.getActiveTokenCount()).thenReturn(activeTokenCount);
+        when(queueRepository.getWaitingTokens(neededTokens)).thenReturn(waitingTokens);
+
+        ArgumentCaptor<Queue> queueCaptor = ArgumentCaptor.forClass(Queue.class);
+
+        // when
+        queueService.updateActiveTokens();
+
+        // then
+        verify(queueRepository, times(1)).getActiveTokenCount();
+        verify(queueRepository, times(1)).getWaitingTokens(neededTokens);
+        verify(queueRepository, times(1)).updateTokenStatusToActive(queueCaptor.capture());
+
+        // 캡처된 Queue 객체의 상태가 ACTIVE로 변경되었는지 검증
+        Queue capturedQueue = queueCaptor.getValue();
+        assertThat(capturedQueue.status()).isEqualTo(QueueStatus.ACTIVE);
+    }
+
 }
