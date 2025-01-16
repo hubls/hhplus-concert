d8bb3f37ab9409c6d242877bda219e28a9a85770
leesangjun <hubls@naver.com>
2025-01-16 14:38:13 +0900

    🚨 Test: 사용자가 결제를 여러번 시도할 시 한 번만 결제 성공하는 통합테스트

--- src/main/java/io/hhplus/concert/hhplusconcert/infra/repository/jpa/ReservationJpaRepository.java
+++ src/main/java/io/hhplus/concert/hhplusconcert/infra/repository/jpa/ReservationJpaRepository.java
@@ -3,7 +3,9 @@ package io.hhplus.concert.hhplusconcert.infra.repository.jpa;
 
 import io.hhplus.concert.hhplusconcert.infra.entity.ReservationEntity;
 import io.hhplus.concert.hhplusconcert.support.type.ReservationStatus;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
@@ -12,6 +14,7 @@ import java.util.Optional;
 
 public interface ReservationJpaRepository extends JpaRepository<ReservationEntity, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select r from reservation r where r.id = ?1")
     Optional<ReservationEntity> findByReservationId(Long reservationId);
 
